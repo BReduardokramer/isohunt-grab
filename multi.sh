1adb6a6149cf11b9d4011acbabd38e1512356643
Sven Slootweg <jamsoftgamedev@gmail.com>
2013-10-20 22:56:04 +0200

    pipeline-multi.py patch

--- multi.sh
+++ multi.sh
@@ -2,9 +2,9 @@ nick=YOURNICKHERE
 oldip=x.x.x.x
 
 for i in `cat ips`;  do
-    sed s/$oldip/$i/ pipeline.py -i
-    nohup run-pipeline pipeline.py $nick --concurrent 6 --disable-web-server > $i.log &
+    sed s/$oldip/$i/ pipeline-multi.py -i
+    nohup run-pipeline pipeline-multi.py $nick --concurrent 6 --disable-web-server > $i.log &
 	oldip=$i
 done
 
-sed s/$oldip/x.x.x.x/ pipeline.py -i
+sed s/$oldip/x.x.x.x/ pipeline-multi.py -i
